10f0d866a55ff1c733c3a96b64b97ecb86ee2af1
Rui Gusmão <rui_gus@hotmail.com>
2021-03-26 22:48:25 +0000

    removed everything

--- LuaRules/Configs/maker_defs.lua
+++
@@ -1,9 +0,0 @@
-local convertCapacities = {
-		[UnitDefNames.aven_metal_maker.id]  = { c = 360, e = 1/144 },
-		[UnitDefNames.gear_metal_maker.id]  = { c = 360, e = 1/144 },
-		[UnitDefNames.claw_metal_maker.id]  = { c = 360, e = 1/144 },
-		[UnitDefNames.sphere_metal_maker.id]  = { c = 360, e = 1/144 },
-    }
-
-return convertCapacities
-
--- LuaRules/Gadgets/ai/Modules.lua
+++
@@ -1,4 +0,0 @@
-include("luarules/gadgets/ai/UnitHandler.lua")
-include("luarules/gadgets/ai/BuildSiteHandler.lua")
-
-modules = { UnitHandler, BuildSiteHandler }
